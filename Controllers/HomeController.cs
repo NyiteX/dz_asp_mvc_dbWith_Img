36588043c2cb35b9c54ef2e29efa8a346a088201
NyiteX <109142191+NyiteX@users.noreply.github.com>
2023-08-20 10:57:42 +0300

    cookie, login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using dz_asp_mvc_db.Classes;
 using dz_asp_mvc_db.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace dz_asp_mvc_db.Controllers
 {
@@ -18,11 +22,61 @@ namespace dz_asp_mvc_db.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public IActionResult Index()
         {
             List<UserModel> users = _context.Users.ToList();
             return View(users);
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string name, string password)
+        {
+            bool f = _context.Users.Any(user => user.Login == name && user.Password == HashClass.ToSHA256(password));
+            if (!f)
+            {
+                ModelState.AddModelError("", "Wrong login or password.");
+                return View();
+            }
+
+           /* int? userId = _context.Users
+                        .Where(user => user.Login == name && user.Password == HashClass.ToSHA256(password))
+                        .Select(user => user.Id)
+                        .FirstOrDefault();*/
+
+            var claims = new List<Claim>
+            {
+                /*new Claim(ClaimTypes.NameIdentifier, userId.ToString()),*/
+                new Claim(ClaimTypes.Name, name),
+                new Claim(ClaimTypes.Email, password)
+            };
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
+        }
+
 
         [HttpGet]
         public IActionResult Registration()
@@ -31,6 +85,7 @@ namespace dz_asp_mvc_db.Controllers
         }
 
         [HttpPost]
+        /*public async Task<IActionResult> Registration(string name, string password, string email)*/
         public IActionResult Registration(string name, string password, string email)
         {
             byte[] pic;
@@ -47,7 +102,7 @@ namespace dz_asp_mvc_db.Controllers
             _context.Users.Add(user);
             _context.SaveChanges();
 
-            return View("Index", _context.Users.ToList());
+            return View("Index", _context.Users.ToList());          
         }
 
         public IActionResult Privacy()
--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 using dz_asp_mvc_db.Classes;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//db
 string connection = @"Data Source = WIN-U669V8L9R5E; Initial Catalog = UserASP; Trusted_Connection=True; TrustServerCertificate = True";
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 
+//печеньки
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => 
+{
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(10); // ѕродолжительность срока действи€ куки
+    options.LoginPath = "/Home/Login"; // ѕуть к странице авторизации
+    options.SlidingExpiration = true;
+});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -24,13 +36,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-
-/*app.UseAuthorization();
-app.UseAuthentication();*/
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-/*pattern: "{controller=Home}/{action=Index}/{id?}");*/
-pattern: "{controller=Home}/{action=Registration}/{id?}");
+pattern: "{controller=Home}/{action=Index}/{id?}");
+/*pattern: "{controller=Home}/{action=Registration}/{id?}");*/
 
 app.Run();
